5741ae850259cb2e3f056c898c12593db1719f5d
Knut B <knut.berling@gmx.de>
2024-09-23 13:13:19 +0200

    changed StructuralPlotEval to calculate percentage over all resources, and not take the mean of the percentage of each resource

--- Evaluation/StructuralMetrics/PlotStructuralResults.py
+++ Evaluation/StructuralMetrics/PlotStructuralResults.py
@@ -21,8 +21,6 @@ pd.options.plotting.backend = "plotly"
 # Calculate the average of each metric for dynamic and static in total
 def plot_std_dev_over_runs(res_df,PLOT_FIG):
     res_df = res_df[['db_config_id', 'metric', 'dynamic', 'overlap_perc', 'uncertain_mappings']]
-    group_df = res_df.groupby(['metric', 'dynamic'])
-    #print(group_df.mean(numeric_only=True))
     gb_db = res_df.groupby(['metric', 'dynamic','db_config_id'])
     gb_db = gb_db.var(numeric_only=True)
     gb_db = gb_db.reset_index()
@@ -35,40 +33,38 @@ def plot_std_dev_over_runs(res_df,PLOT_FIG):
         fig.show()
         fig.write_image("plots/std_dev_of_metrics_over_3_runs.png")
     return r[['metric','dynamic','std_dev']]
+
 def plot_uncertain_mappings(res_df,PLOT_FIG):
     res_df = res_df[['db_config_id', 'metric', 'dynamic', 'uncertain_mappings']]
 
     gb_metric = res_df.groupby(['metric', 'dynamic'])
-    r = gb_metric.mean(numeric_only=True)
+    r = gb_metric.sum(numeric_only=True)
     r = r.reset_index()
     if PLOT_FIG:
         fig = px.bar(r, x='metric', y='uncertain_mappings', color='dynamic', barmode="group")
         fig.show()
     return r[['metric','dynamic','uncertain_mappings']]
 
-def plot_overlap_perc_all_metrics(res_df,PLOT_FIG):
-    """"""
-    df = res_df[['dynamic', 'metric', 'overlap_perc','runtime']]
+def calc_rt_average(res_df):
+    df = res_df[['dynamic', 'metric','runtime']]
     group_df = df.groupby(['metric', 'dynamic'])
     mean_df = group_df.mean(numeric_only=True)
     mean_df = mean_df.reset_index()
+    return mean_df[['metric','dynamic','runtime']]
+
+def plot_overlap_perc_all_metrics(res_df,nr_total_records,PLOT_FIG):
+    """ Calculate the average by collecting common records  and divide by nr of constants (using overlap perc. is inaccurate)"""
+    df = res_df[['dynamic', 'metric', 'common_records']]
+    group_df = df.groupby(['metric', 'dynamic'])
+    sum_df = group_df.sum(numeric_only=True)
+    sum_df = sum_df.reset_index()
     if PLOT_FIG:
-        fig = px.bar(mean_df,x='metric',y="overlap_perc",barmode="group",color="dynamic",title="Overlap Percentage")
+        fig = px.bar(sum_df,x='metric',y="overlap_perc",barmode="group",color="dynamic",title="Overlap Percentage")
         fig.show()
-    return mean_df[['metric','dynamic','overlap_perc','runtime']]
+    # Divide Sum by nr of records per resource & run
+    sum_df['overlap_perc'] = sum_df['common_records'] * 100 / (nr_total_records * 5) # 5 iterations per resource
 
-def plot_dynamic_overlap_perc_for_resource(res_df, config_df,PLOT_FIG):
-    df = res_df[res_df['dynamic'] == "True"]
-    df = df[['metric','overlap_perc','db_config_id']]
-    df = df.merge(config_df, left_on='db_config_id', right_on='db_config_id')
-    df = df.groupby(['file_name', 'metric', 'db1'])
-    df = df.mean(numeric_only=True)
-    df.reset_index(inplace=True)
-    if PLOT_FIG:
-        fig = px.bar(df,x='file_name',y="overlap_perc",barmode="group",color="metric")
-        fig.show()
-
-
+    return sum_df
 
 
 def count_correct_mappings(res_df, config_df):
@@ -95,16 +91,20 @@ def count_correct_mappings(res_df, config_df):
         else:
             print(f"file does not exist: {mapping_file}")
 
-    res_df['avg_corr_mappings'] = 100 * res_df['corr_mappings'] / res_df['total_mappings']
 
 
     nr_mappings_df = res_df[['file_name','db1','total_mappings']]
-    res_df = res_df[['db_config_id', 'metric', 'dynamic','avg_corr_mappings']]
+    nr_total_mappings = sum(nr_mappings_df['total_mappings']) / 6 # this sums over all metrics and runs so divide by 6 metrics
+
+
     group_df = res_df.groupby(['metric','dynamic'])
-    df = group_df.mean(numeric_only=True)
-    df.reset_index(inplace=True)
-    nr_mappings_df.drop_duplicates(inplace=True,ignore_index=True)
-    return df[['metric','dynamic','avg_corr_mappings']],nr_mappings_df
+    group_df = group_df.sum(numeric_only=True)
+    group_df = group_df.reset_index()
+    group_df['avg_corr_mappings'] = group_df['corr_mappings'] * 100 / nr_total_mappings
+
+
+    nr_mappings_df = nr_mappings_df.drop_duplicates(ignore_index=True)
+    return group_df[['metric','dynamic','avg_corr_mappings']],nr_mappings_df,nr_total_mappings
 
 
 def count_facts_per_db(config_df):
@@ -119,37 +119,49 @@ def count_facts_per_db(config_df):
         nr_facts = count_facts_in_dir(fact_path)
         s = pd.Series({'file_name':row['file_name'],'nr_facts':nr_facts})
         facts_df = add_series_to_df(series=s,df=facts_df)
-    return facts_df
+
+    nr_total_records = sum(facts_df['nr_facts'])
+    return facts_df,nr_total_records
 
 if __name__ == "__main__":
     PLOT_FIG = False
     res_df = sql_con.get_table(table="StructuralResults")
     mapping_df = sql_con.get_table(table="MappingSetup")
     res_df = res_df.merge(mapping_df, left_on='mapping_id', right_on='mapping_id')
-    mean_df = plot_overlap_perc_all_metrics(res_df,PLOT_FIG)
+    config_df = sql_con.get_table(table="DbConfig")
+
+    # This df holds characteristics of each database
+    facts_df,nr_total_records = count_facts_per_db(config_df)
+
+    # This collects the sum of correct mapped RECORDS per metric and is normalised with nr_total_records
+    overlap_df = plot_overlap_perc_all_metrics(res_df,nr_total_records,PLOT_FIG)
+
+    runtime_df = calc_rt_average(res_df)
 
     std_dev_df = plot_std_dev_over_runs(res_df,PLOT_FIG)
-    df = pd.merge(left=mean_df,right=std_dev_df,left_on=['metric','dynamic'],
-                  right_on=['metric','dynamic'])
+
+    df = pd.merge(left=runtime_df,right=std_dev_df,on=['metric','dynamic'])
+    df = pd.merge(overlap_df,df,on=['metric','dynamic'])
 
     uncertain_df = plot_uncertain_mappings(res_df,PLOT_FIG)
-    df = pd.merge(left=df,right=uncertain_df,left_on=['metric','dynamic'],
-                  right_on=['metric','dynamic'])
+    df = pd.merge(left=df,right=uncertain_df,on=['metric','dynamic'])
 
-    config_df = sql_con.get_table(table="DbConfig")
-    corr_mappings_df,resource_df = count_correct_mappings(res_df, config_df)
-    facts_df = count_facts_per_db(config_df)
+    corr_mappings_df,resource_df,nr_total_mappings = count_correct_mappings(res_df, config_df)
+
+
+
     resource_df = pd.merge(resource_df,facts_df,on='file_name')
+    sql_con.insert_df(table="DatabaseCharacteristics", df=resource_df, write_index=False)
 
-    df = pd.merge(left=df, right=corr_mappings_df, left_on=['metric', 'dynamic'],
-                  right_on=['metric', 'dynamic'])
+    df = pd.merge(left=df, right=corr_mappings_df, on=['metric', 'dynamic'])
+
     # Update uncertain mappings to a percentage, by dividing through nr of all constants
-    nr_total_mappings = np.sum(resource_df['total_mappings'])
-    df['avg_uncertain_mappings'] = df['uncertain_mappings'] * 100 / nr_total_mappings
+    df['avg_uncertain_mappings'] = df['uncertain_mappings'] * 100 / (nr_total_mappings)
+
     df = df.round(2)
+
     # Change order of columns slightly
     df = df[['metric','dynamic','overlap_perc','avg_corr_mappings','avg_uncertain_mappings','std_dev','runtime']]
     print(df.to_latex())
     print(resource_df.to_latex())
-    #plot_dynamic_overlap_perc_for_resource(res_df, config_df)
 
